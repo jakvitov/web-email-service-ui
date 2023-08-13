0d409720bf070815837804fc27b6c768dc0cbf8a
Jakub Vítovec <jakvitov@gmail.com>
2023-08-13 21:08:27 +0200

    TraceId generation implemented

--- src/components/UserBackendInfo.js
+++ src/components/UserBackendInfo.js
@@ -1,37 +1,43 @@
 import {useEffect, useState} from "react";
 import {useAuth0} from "@auth0/auth0-react";
 import axios from "axios";
+const { v4: uuidv4 } = require('uuid');
 
 const UserBackendInfo = () => {
 
     const {user, isAuthenticated, isLoading, getIdTokenClaims} = useAuth0();
     const [userInfoFromBackend, setUserInfoFromBackend] = useState(null);
 
-    useEffect(async () => {
-        const tokenRes = await getIdTokenClaims();
-        const token = tokenRes.__raw;
-        console.log(token)
-        const axiosConfig = {
-            headers: {
-                Authorization: "Bearer " + token
+    useEffect( () => {
+        async function fetchData(){
+            const tokenRes = await getIdTokenClaims();
+            const token = tokenRes.__raw;
+            const axiosConfig = {
+                headers: {
+                    Authorization: "Bearer " + token,
+                    TRACE_ID: uuidv4()
+                }
             }
+            const userInfoResponse = axios.get("/user/info", axiosConfig).then(response => setUserInfoFromBackend(response.data)).catch(err => console.log(err))
         }
-        const userInfoResponse = axios.get("/user/info", axiosConfig).then(response => setUserInfoFromBackend(response.data)).catch(err => console.log(err))
+    fetchData()
     }, [isAuthenticated, isLoading])
 
     if (isLoading || !isAuthenticated){
-        console.log("1")
         return null;
     }
     else  if (userInfoFromBackend == null){
-        console.log("2")
         return null;
     }
+    else if (userInfoFromBackend.responseState != "OK")
+        return <p>{userInfoFromBackend.responseState}</p>
     else {
         return (
         <div id="userInfoFromBackendDiv">
             <p>City: {userInfoFromBackend.city}</p>
             <p>Country: {userInfoFromBackend.countryISO}</p>
+            <p>Longitude: {userInfoFromBackend.longitude}</p>
+            <p>Latitude: {userInfoFromBackend.latitude}</p>
         </div>
         )
     }
